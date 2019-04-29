0fa53ebb33d9be403035eb38ba7a4adda97fc8d7
Eunhee Jo <joeun2ce@gmail.com>
2019-04-29 17:57:06 +0900

    delete handler

--- migrationSequences.js
+++
@@ -1,9 +0,0 @@
-const { Migration } = require("demux-postgres")
-
-const gxcAction = new Migration(
-  "gxcAction", "gxc", "./gxcontract.sql")
-
-module.exports = [{
-  migrations: [gxcAction],
-  sequenceName: "init"
-}]
