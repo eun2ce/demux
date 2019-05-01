554528e24eb3249f4c79a63f8cc53dc9b9e1c0b0
Eunhee Jo <joeun2ce@gmail.com>
2019-05-01 18:37:00 +0900

    Update to the log style of the Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { BaseActionWatcher } from "demux";
-import { config } from "dotenv";
-import { handlerVersions } from "./handlerVersions/v1";
 import { MongoActionReader } from "demux-eos";
 import { MassiveActionHandler } from "demux-postgres";
-import massive = require("massive");
+import { handlerVersions } from "./handlerVersions/v1";
 import { migrationSequences } from "./migrationSequences";
 
+import { config } from "dotenv";
+import massive = require("massive");
+import * as pino from "pino";
+
 config();
+const logger = pino({prettyPrint: true});
+
 const dbConfig: massive.ConnectionInfo = {
    database: process.env.POSTGRES_DBNAME || "GXC",
    host: process.env.POSTGRES_HOST || "127.0.0.1",
@@ -15,7 +19,7 @@ const dbConfig: massive.ConnectionInfo = {
    schema: process.env.POSTGRES_SCHEMA || "gxc",
    user: process.env.POSTGRES_UNAME || "postgres",
 };
-console.info("DB Config >>>\n", dbConfig);
+logger.info("DB Config >>", dbConfig);
 
 const init = async () => {
 
@@ -27,7 +31,7 @@ const init = async () => {
             return(result === null) ? 1 : result.block_number ++;
         })
     : 1;
-   console.info("Startblock Number is ", startBlock);
+   logger.info("Startblock Number is ", startBlock);
 
    const actionReader: any = new MongoActionReader(
       {
@@ -50,15 +54,15 @@ const init = async () => {
       const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 500);
 
       actionWatcher.watch();
-      console.info(`Demux listening on ${ dbConfig.port }port..`);
+      logger.info(`Demux listening on ${ dbConfig.port }port..`);
    } catch (e) {
-      console.info(e);
+      logger.info(e);
    }
 };
 
 const exit = (e: any) => {
-  console.error("An error has occured. error is: %s and stack trace is: %s", e, e.stack);
-  console.error("Process will restart now.");
+  logger.error("An error has occured. error is: %s and stack trace is: %s", e, e.stack);
+  logger.error("Process will restart now.");
   process.exit(1);
 };
 
